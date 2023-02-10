40e5ca2150e48174f1bad03b7150b4517d4ad6b7
Isaac Marovitz <isaacryu@icloud.com>
2023-02-09 20:14:55 -0500

    Google Analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,19 @@
 import { defineConfig } from 'astro/config';
-import UnoCSS from 'unocss/astro'
+import UnoCSS from 'unocss/astro';
 import vue from "@astrojs/vue";
+
+// https://astro.build/config
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
-    vue(),
-    UnoCSS()
+    vue(), 
+    UnoCSS(), 
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    })
   ]
 });
