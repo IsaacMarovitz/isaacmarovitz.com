f6755f6b80270f5fe6814907782ff02561d1f57a
Isaac Marovitz <isaacryu@icloud.com>
2025-10-06 19:20:18 -0400

    Fix typo
    Signed-off-by: Isaac Marovitz <isaacryu@icloud.com>

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -6,6 +6,6 @@ export async function GET(context) {
     description: "Hi, I'm Isaac. I'm a developer from London. I code games, apps, and websites.",
     site: context.site,
     items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
-    customData: `<language>en-gn</language>`,
+    customData: `<language>en-gb</language>`,
   });
 }
