139797985f6c765097f9a093d19e2f233a7786ab
Isaac Marovitz <isaacryu@icloud.com>
2025-10-24 13:54:45 -0400

    Fix RSS feed
    Signed-off-by: Isaac Marovitz <isaacryu@icloud.com>

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,11 +1,18 @@
 import rss, { pagesGlobToRssItems } from "@astrojs/rss";
+import { getCollection } from 'astro:content';
 
 export async function GET(context) {
+  const posts = await getCollection("posts");
   return rss({
     title: "Isaac Marovitz - Blog",
     description: "Hi, I'm Isaac. I'm a developer from London. I code games, apps, and websites.",
     site: context.site,
-    items: await pagesGlobToRssItems(import.meta.glob('./**/*.mdx')),
+    items: posts.map((post) => ({
+      title: post.data.title,
+      pubDate: post.data.pubDate,
+      description: post.data.description,
+      link: `/posts/${post.id}`
+    })),
     customData: `<language>en-gb</language>`,
   });
 }
