f620b142c65eee06836de9b0d835dcd7ae143bab
Isaac Marovitz <isaacmarovitz@proton.me>
2026-04-04 22:26:24 +0100

    Disable CSP

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,6 @@ import mdx from "@astrojs/mdx";
 // https://astro.build/config
 export default defineConfig({
   site: "https://isaacmarovitz.com",
-  security: { csp: true },
   integrations: [
     mdx(),
     partytown({
